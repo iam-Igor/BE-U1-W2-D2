63ff72640968f96c7f52e381739bd6c537431f38
Ygor Garofalo <igorgarofalo@gmail.com>
2023-12-05 17:33:57 +0100

    es3 ok

--- src/es3/Main3.java
+++ src/es3/Main3.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class Main3 {
     private static Logger log = LoggerFactory.getLogger(Main3.class);
@@ -25,22 +24,30 @@ public class Main3 {
 
         rubrica.remove("Luigi");
 
-        log.info(rubrica.toString());
-
-        searchUserByNumber(333221150, rubrica);
-
+        searchByname("Mario", rubrica);
+        searchByNumber(333211662, rubrica);
 
     }
 
-    private static void searchUserByNumber(int numero, Map<String, Integer> contacts) {
-
-        Set<String> elencoChiavi = contacts.keySet();
-        
-        for (Integer num : elencoChiavi) {
-            System.out.println("Chiave " + num);
-            System.out.println("Valore " + contacts.get(num));
+    public static void searchByname(String name, Map<String, Integer> contacts) {
+        if (contacts.containsKey(name)) {
+            int numero = contacts.get(name);
+            log.info(name + numero);
+        } else {
+            log.info(name + " non è presente nella rubrica.");
         }
-
     }
 
+    public static void searchByNumber(int number, Map<String, Integer> contacts) {
+        if (contacts.containsValue(number)) {
+            for (Map.Entry<String, Integer> entry : contacts.entrySet()) {
+                if (entry.getValue() == number) {
+                    log.info("Il numero " + number + " appartiene a: " + entry.getKey());
+                    return; // Termina la funzione una volta trovato il numero
+                }
+            }
+        } else {
+            log.info("Il numero " + number + " non è associato a nessun contatto nella rubrica.");
+        }
+    }
 }
